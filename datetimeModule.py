898724933b42846debfd28267bbd1e2bd6c7223d
MoonAditya <41886101+MoonAditya@users.noreply.github.com>
2020-09-27 23:47:36 -0400

    Update datetimeModule.py
    removed space

--- datetimeModule.py
+++ datetimeModule.py
@@ -8,5 +8,5 @@ print("The current UTC datetime is", c.strftime("%m/%d/%Y, %H:%M:%S %p"))
 fmt = '%Y-%m-%d %H:%M:%S %Z%z' #24 hours
 fmt1= '%m/%d/%Y, %I:%M:%S %p %Z%z' #12 hours
 
-print ("The current EST datetime in 24 hours format is ", c.astimezone(est).strftime(fmt))
-print ("The current EST datetime in 12 hours format is ", c.astimezone(est).strftime(fmt1))
+print("The current EST datetime in 24 hours format is ", c.astimezone(est).strftime(fmt))
+print("The current EST datetime in 12 hours format is ", c.astimezone(est).strftime(fmt1))
